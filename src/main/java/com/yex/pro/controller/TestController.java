a7672e84710c63502013f75164a89171c5c7de14
yexuan <1147736795@qq.com>
2020-04-07 15:36:22 +0800

    commit static

--- src/main/java/com/yex/pro/controller/TestController.java
+++
@@ -1,8 +0,0 @@
-package com.yex.pro.controller;
-
-/**
- * Created by yexuan on 2020/3/9.
- */
-
-public class TestController {
-}
---
+++ src/main/java/com/yex/pro/controller/sys/UserController.java
@@ -0,0 +1,7 @@
+package com.yex.pro.controller.sys;
+
+/**
+ * Created by yexuan on 2020/4/3.
+ */
+public class UserController {
+}
