870acc7d3585e87aea8632e97b97a43249cd728b
yexuan <1147736795@qq.com>
2020-04-07 15:42:25 +0800

    modif code

--- src/main/java/com/yex/pro/controller/sys/UserController.java
+++ src/main/java/com/yex/pro/controller/sys/UserController.java
@@ -1,7 +1,17 @@
 package com.yex.pro.controller.sys;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by yexuan on 2020/4/3.
  */
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @RequestMapping("/getById")
+    public String getUserById(){
+        return "/user/profile";
+    }
 }
